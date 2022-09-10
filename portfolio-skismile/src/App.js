8ff658070583ed0ba12d954290ceb16e2ceb0297
skismile <sk.ismile75@gmail.com>
2022-09-10 22:29:17 +0530

    add

---
+++ portfolio-skismile/src/App.js
@@ -0,0 +1,16 @@
+
+import Profile from './PortfolioContainer/Home/Profile'
+import  "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+    
+      
+      <Profile/>
+
+          </div>
+  );
+}
+
+export default App;
